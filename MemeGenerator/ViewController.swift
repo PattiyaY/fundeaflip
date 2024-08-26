e10a1d679ac4a90f8fa730043a540716b166bb8a
PattiyaY <pattiya.2547@icloud.com>
2024-08-26 15:20:56 +0700

    chore: initial commit

---
+++ MemeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeGenerator
+//
+//  Created by Pattiya Yiadram on 26/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
