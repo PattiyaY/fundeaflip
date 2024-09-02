66c5dfac74e7904388f73f27f0f1a711ccfd27e0
Jan Poonthong <janpoonthong628@gmail.com>
2024-09-02 18:29:12 +0700

    feat: navigation bar appearance

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -10,6 +10,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let appearance = UINavigationBarAppearance()
+                appearance.backgroundColor = .black // Set the background color to black
+                appearance.titleTextAttributes = [.foregroundColor: UIColor.white] // Set the title color to white
+                appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white] // Set the large title color to white
+                
+                // Apply the appearance to the navigation bar for both standard and scroll edge
+                navigationController?.navigationBar.standardAppearance = appearance
+                navigationController?.navigationBar.scrollEdgeAppearance = appearance
+                
+                // Enable large titles if desired
+                navigationController?.navigationBar.prefersLargeTitles = true
+        
         collectionView.delegate = self
         collectionView.dataSource = self
         
